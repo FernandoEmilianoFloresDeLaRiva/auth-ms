8150975d5120618c13122b559ecf196e52c6aa46
FernandoEmilianoFloresDeLaRiva <emilianoflores07081@gmail.com>
2025-05-15 11:11:58 -0600

    first commit

---
+++ src/main.ts
@@ -0,0 +1,19 @@
+import { NestFactory } from '@nestjs/core';
+import { AppModule } from './app.module';
+import { Logger, ValidationPipe } from '@nestjs/common';
+
+async function bootstrap() {
+  const logger = new Logger('Auth MS');
+  const app = await NestFactory.create(AppModule);
+  app.enableCors();
+  app.useGlobalPipes(
+    new ValidationPipe({
+      whitelist: true,
+      forbidNonWhitelisted: true,
+      transform: true,
+    }),
+  );
+  await app.listen(3000);
+  logger.log(`Auth MS is running on: ${app.getUrl()}`);
+}
+bootstrap();
